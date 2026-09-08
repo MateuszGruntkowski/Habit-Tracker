e21b29e764c92b2832f0da05848ede8b3ccdc289
Mateusz Gruntkowski <mati.gruntkowski@gmail.com>
2026-09-08 17:06:16 +0200

    Enhance App component with week navigation; update Header and HabitList to utilize visibleDates and improve state management with useLocalStorage

--- src/context/HabitProvider.tsx
+++ src/context/HabitProvider.tsx
@@ -1,13 +1,14 @@
-import { type ReactNode, useState } from "react";
+import { type ReactNode } from "react";
 import { isSameDay } from "date-fns";
 import { HabitContext, type Habit } from "./useHabits";
+import { useLocalStorage } from "../hooks/useLocalStorage";
 
 type HabitProviderProps = {
   children: ReactNode;
 };
 
 export function HabitProvider({ children }: HabitProviderProps) {
-  const [habits, setHabits] = useState<Habit[]>([]);
+  const [habits, setHabits] = useLocalStorage<Habit[]>("Habits", []);
 
   function addHabit(name: string) {
     setHabits((curr) => [
